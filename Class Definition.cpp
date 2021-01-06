b5d35cd6205f758e375ec8e6e1cc184e0bcdb2ed
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-06 23:42:30 +0600

    class definition

---
+++ Class Definition.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+class Circle
+{         // classname
+private:
+   double radius;      // Data members (variables)
+   string color;
+public:
+   double getRadius(); // Member functions
+   double getArea();
+};
+class SoccerPlayer {   // classname
+private:
+   int number;         // Data members (variables)
+   string name;
+   int x, y;
+public:
+   void run();         // Member functions
+   void kickBall();
+};
+
+    return 0;
+}
