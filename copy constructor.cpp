a2abebd3471bdaca97ac795b157e43d1185d3601
mohsin0176 <mohsin2010ju@gmail.com>
2021-01-07 19:36:14 +0600

    copy constructor

--- copy constructor.cpp
+++ copy constructor.cpp
@@ -21,6 +21,8 @@ public:
 int main()
 {
 Circle c6(5.6, "orange"), c7;
+c7 = c6; // memberwise copy assignment
+Circle c8 = c6;
 cout << "Radius=" << c6.getRadius() << " Area=" << c6.getArea()
      << " Color=" << c6.getColor() << endl;
                 // Radius=5.6 Area=98.5206 Color=orange
@@ -28,7 +30,7 @@ cout << "Radius=" << c7.getRadius() << " Area=" << c7.getArea()
      << " Color=" << c7.getColor() << endl;
                 // Radius=1 Area=3.1416 Color=red (default constructor)
 
-c7 = c6; // memberwise copy assignment
+
 cout << "Radius=" << c7.getRadius() << " Area=" << c7.getArea()
      << " Color=" << c7.getColor() << endl;
                 // Radius=5.6 Area=98.5206 Color=orange
